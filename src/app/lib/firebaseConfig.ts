fcedafe6f417183f6d40d42af413df6169212012
heloysa <112661800+helxysa@users.noreply.github.com>
2024-11-18 09:55:40 -0300

    Update firebaseConfig.ts

--- src/app/lib/firebaseConfig.ts
+++ src/app/lib/firebaseConfig.ts
@@ -5,15 +5,6 @@ import { getFirestore } from "firebase/firestore";
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDcM-n7rOE93hjJvmOeZNbz4mW1-C_gWgg",
-  authDomain: "organizador-de-tarefas-87701.firebaseapp.com",
-  projectId: "organizador-de-tarefas-87701",
-  storageBucket: "organizador-de-tarefas-87701.firebasestorage.app",
-  messagingSenderId: "480713926627",
-  appId: "1:480713926627:web:02618f3cc050380e9f62d6",
-  measurementId: "G-XKD9WNELDG"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
